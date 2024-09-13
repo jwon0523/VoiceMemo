1fc00604d7fd032cf1be2c5b486bcb913d76ecc4
jwon0523 <jwon010523@gmail.com>
2024-09-13 22:16:43 +0900

    TimerViewModel done

--- voiceMemo/Feature/Home/Timer/TimerViewModel.swift
+++ voiceMemo/Feature/Home/Timer/TimerViewModel.swift
@@ -5,6 +5,71 @@
 
 import Foundation
 
-class TimerViewModel {
+class TimerViewModel: ObservableObject {
+  @Published var isDisplaySetTimeView: Bool
+  @Published var time: Time
+  @Published var timer: Timer?
+  @Published var timeRemaining: Int
+  @Published var isPaused: Bool
   
+  init(
+    isDisplaySetTimeView: Bool = true,
+    time: Time = .init(hours: 0, minutes: 0, seconds: 0),
+    timer: Timer? = nil,
+    timeRemaining: Int = 0,
+    isPaused: Bool = false
+  ) {
+    self.isDisplaySetTimeView = isDisplaySetTimeView
+    self.time = time
+    self.timer = timer
+    self.timeRemaining = timeRemaining
+    self.isPaused = isPaused
+  }
+}
+
+extension TimerViewModel {
+  func settingBtnTapped() {
+    isDisplaySetTimeView = false
+    timeRemaining = time.convertedSeconds
+    // TODO: - 타이머 시작 메서드 호출
+  }
+  
+  func cancelBtnTagged() {
+    // TODO: - 타이머 종료 메서드 호출
+    isDisplaySetTimeView = true
+  }
+  
+  func pauseOrRestartBtnTapped() {
+    if isPaused {
+      // TODO: - 타이머 시작 메서드 호출
+    } else {
+      timer?.invalidate()
+      timer = nil
+    }
+    isPaused.toggle()
+  }
+}
+
+private extension TimerViewModel {
+  func startTimer() {
+    guard timer == nil else { return }
+    timer = Timer.scheduledTimer(
+      withTimeInterval: 1,
+      repeats: true
+    ) { [weak self]_ in
+      // 클로저 내에 self를 명시하여 Timer 객체가 아닌
+      // TimerViewModel를 참조하는 것임을 명확히 해줘야 함
+      guard let self = self else { return } // 객체가 해제되었는지 확인
+      if self.timeRemaining > 0 {
+        self.timeRemaining -= 1
+      } else {
+        self.stopTimer()
+      }
+    }
+  }
+  
+  func stopTimer() {
+    timer?.invalidate()
+    timer = nil
+  }
 }
